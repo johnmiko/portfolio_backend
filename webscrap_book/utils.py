97f107c0dee5cd33c1ea92af5ee4636fd501c610
johnmiko <johnmiko4@gmail.com>
2022-12-24 15:10:18 -0500

    make website class abstract

--- webscrap_book/utils.py
+++ webscrap_book/utils.py
@@ -1,6 +1,22 @@
+from abc import ABC, abstractmethod
 from enum import Enum
 
 from selenium.webdriver.support.wait import WebDriverWait
+
+
+class Websites(Enum):
+    all_free_novel = 'https://www.allfreenovel.com/Page/Story/'
+    gemibook = 'https://gemibook.com/'
+
+
+class WebsiteFactory:
+    @staticmethod
+    def create(name):
+        if name == Websites.all_free_novel:
+            return AllFreeNovelWebsite()
+        elif name == Websites.gemibook:
+            return GemiBookWebsite()
+        # https://gemibook.com/project-hail-mary/p-1-10077539
 
 
 class Book:
@@ -11,29 +27,45 @@ class Book:
         self.name_dash = name.replace(' ', '-')
 
 
-class Website:
-    def __init__(self, base_url):
-        self.base_url = base_url
+class Website(ABC):
+    @abstractmethod
+    def create_url(self, book, page_number, total_pages):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_text(self, driver):
+        raise NotImplementedError()
+
+
+class AllFreeNovelWebsite(Website):
+    base_url = Websites.all_free_novel.value
 
     def create_url(self, book, page_number, total_pages):
         return f'{self.base_url}{book.number}/page-{page_number}-{book.name_dash}/{page_number}/{total_pages}'
 
     def get_text(self, driver):
-        all_text_els = WebDriverWait(driver, 1).until(lambda driver:
-                                                      driver.find_elements('xpath',
-                                                                           "//p[@class='storyText story-text']"))
-        all_text = [text_el.text for text_el in all_text_els]
-        all_text2 = '\n'.join(all_text)
-        all_text2 = all_text2.encode('latin1', 'ignore').decode("latin1")
-        return all_text2
+        text_els = WebDriverWait(driver, 1).until(lambda driver:
+                                                  driver.find_elements('xpath',
+                                                                       "//p[@class='storyText story-text']"))
+        text_list = [text_el.text for text_el in text_els]
+        text = '\n'.join(text_list)
+        text2 = text.encode('latin1', 'ignore').decode("latin1")
+        return text2
 
 
-class Websites(Enum):
-    all_free_novel = 'https://www.allfreenovel.com/Page/Story/'
+class GemiBookWebsite(Website):
+    base_url = Websites.gemibook.value
 
+    def create_url(self, book, page_number, total_pages):
+        # https://gemibook.com/project-hail-mary/p-1-10077539
+        # https://gemibook.com//Project-Hail-Mary/p-1-10077539
+        return f'{self.base_url}{book.name_dash.lower()}/p-{page_number}-{int(book.number) + int(page_number) - 1}'
 
-class WebsiteFactory:
-    @staticmethod
-    def create(name):
-        if name == Websites.all_free_novel:
-            return Website(Websites.all_free_novel.value)
+    def get_text(self, driver):
+        text_elements = WebDriverWait(driver, 1).until(lambda driver:
+                                                       driver.find_elements('xpath',
+                                                                            "//div[@class='chapter-content']"))
+        text_list = [text_el.text for text_el in text_elements]
+        text = '\n'.join(text_list)
+        text2 = text.encode('latin1', 'ignore').decode("latin1")
+        return text2
