d267a7cc29d19948fcce670d66f79d1a1ef0f3fd
johnmiko <johnmiko4@gmail.com>
2022-12-24 14:49:01 -0500

    generalize book webscraper

--- dota/app/utils.py
+++ dota/app/utils.py
@@ -1,4 +1,4 @@
-from dota.dota.ease_of_use import pdf
+from dota.app.ease_of_use import pdf
 
 print_cols = ['title', 'time_ago']
 
@@ -21,6 +21,15 @@ def print_highlights_df(df, cols, rows=10):
     print('highlights df')
     pdf(df[cols2].head(rows))
     print()
+
+
+def create_wholegame_df(df, cols, rows=10):
+    cols2 = print_cols
+    cols2.extend(cols)
+    cols2 = list(set(cols2))
+    df = df.sort_values('whole_game_score', ascending=False)
+    df = df.set_index('match_id')
+    return df[cols2].head(rows)
 
 
 def print_whole_game_df(df, cols, rows=5):
